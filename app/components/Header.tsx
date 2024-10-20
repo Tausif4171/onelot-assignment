d491e276cecd3c54ea3542b45b8d3b0b7e1e96ce
Tausif4171 <tausifkhan4171@gmail.com>
2024-10-20 23:55:06 +0530

    header responsive ✅

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "./common/Button";
 import logo from "../assets/common/logo.svg";
 import dropdown from "../assets/common/dropdown.svg";
+import toggle from "../assets/Header/toggle.svg";
 import Image from "next/image";
 
 export const Header = () => {
@@ -9,7 +10,7 @@ export const Header = () => {
     <div className=" flex justify-between items-center py-[10px] px-2 md:px-4 mb-[91px]">
       <div className=" flex gap-7 items-center">
         <Image alt="logo" src={logo} />
-        <div className=" flex gap-x-8 items-center">
+        <div className=" hidden md:flex gap-x-8 items-center">
           <h3 className=" text-[#3600D1] font-medium text-[14px]">Home</h3>
           <div className=" flex gap-x-1 items-center">
             <h3 className=" text-[#374151] font-medium text-[14px]">
@@ -21,10 +22,11 @@ export const Header = () => {
           <h3 className=" text-[#374151] font-medium text-[14px]">Contact</h3>
         </div>
       </div>
-      <div className="flex">
+      <div className=" hidden md:flex">
         <Button variant="transparent" text="Login" />
         <Button text="Get Started" />
       </div>
+      <Image className=" md:hidden" src={toggle} alt="" />
     </div>
   );
 };
